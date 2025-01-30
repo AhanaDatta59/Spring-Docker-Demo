342e6f125379d3e665f513f2bdbad912819aa428
AhanaDatta59 <ahanadatta59@gmail.com>
2025-01-30 11:18:20 +0530

    Logging added

--- src/main/java/com/ahana/Docker/Demo/controller/EmployeeController.java
+++ src/main/java/com/ahana/Docker/Demo/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.ahana.Docker.Demo.controller;
 import com.ahana.Docker.Demo.ResouceNotFoundException;
 import com.ahana.Docker.Demo.model.Employee;
 import com.ahana.Docker.Demo.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,22 +15,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeController {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
+
     @Autowired
     private EmployeeRepository employeeRepository;
 
     @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee employee) {
+        LOGGER.info("Inside PostMapping of employees");
         return employeeRepository.save(employee);
     }
 
 
     @GetMapping("/employees")
     public ResponseEntity<List<Employee>> getAllEmployees() {
+        LOGGER.info("Inside get all employees");
         return ResponseEntity.ok(employeeRepository.findAll());
     }
 
     @GetMapping("employees/{id}")
     public ResponseEntity<Employee> findEmployeeById(@PathVariable(value = "id") Integer employeeId) {
+        LOGGER.info("Inside get single employee");
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(
                 () -> new ResouceNotFoundException("Employee not found" + employeeId));
         return ResponseEntity.ok().body(employee);
@@ -37,6 +44,7 @@ public class EmployeeController {
     @PutMapping("employees/{id}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Integer employeeId,
                                                    @RequestBody Employee employeeDetails) {
+        LOGGER.info("Inside update employee");
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(() -> new ResouceNotFoundException("Employee not found for this id :: " + employeeId));
         employee.setName(employeeDetails.getName());
@@ -47,6 +55,7 @@ public class EmployeeController {
 
     @DeleteMapping("employees/{id}")
     public ResponseEntity<Void> deleteEmployee(@PathVariable(value = "id") Integer employeeId) {
+        LOGGER.info("Inside delete employee api");
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(
                 () -> new ResouceNotFoundException("Employee not found" + employeeId));
         employeeRepository.delete(employee);
